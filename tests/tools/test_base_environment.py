Carrie111998/Hermes

fix(terminal): probe remote passwordless sudo

tests/tools/test_base_environment.py
@@ -6,6 +6,7 @@ init_session() failure handling, and the CWD marker contract.
 
 from unittest.mock import MagicMock
 
+import tools.terminal_tool as terminal_tool
 from tools.environments.base import BaseEnvironment, _BoundedOutputCollector
 
 
@@ -20,6 +21,48 @@ class _TestableEnv(BaseEnvironment):
 
     def cleanup(self):
         pass
+
+
+def test_prepare_command_uses_selected_environment_for_nopasswd(monkeypatch):
+    monkeypatch.delenv("SUDO_PASSWORD", raising=False)
+    monkeypatch.setenv("HERMES_INTERACTIVE", "1")
+    env = _TestableEnv()
+    monkeypatch.setattr(env, "_sudo_nopasswd_works", lambda: True)
+
+    def _fail_prompt(*_args, **_kwargs):
+        raise AssertionError("interactive sudo prompt should not run for NOPASSWD")
+
+    monkeypatch.setattr(terminal_tool, "_prompt_for_sudo_password", _fail_prompt)
+
+    transformed, sudo_stdin = env._prepare_command("sudo true")
+
+    assert transformed == "sudo true"
+    assert sudo_stdin is None
+
+
+def test_nopasswd_probe_runs_inside_selected_environment(monkeypatch):
+    env = _TestableEnv()
+    proc = object()
+    run = MagicMock(return_value=proc)
+    wait = MagicMock(return_value={"returncode": 0})
+    monkeypatch.setattr(env, "_run_bash", run)
+    monkeypatch.setattr(env, "_wait_for_process", wait)
+
+    assert env._sudo_nopasswd_works() is True
+    run.assert_called_once_with(
+        "sudo -n true",
+        login=False,
+        timeout=3,
+        stdin_data=None,
+    )
+    wait.assert_called_once_with(proc, timeout=3)
+
+
+def test_nopasswd_probe_fails_closed_on_backend_error(monkeypatch):
+    env = _TestableEnv()
+    monkeypatch.setattr(env, "_run_bash", MagicMock(side_effect=RuntimeError("offline")))
+
+    assert env._sudo_nopasswd_works() is False
 
 
 class TestBoundedOutputCollector:

tests/tools/test_terminal_tool.py
@@ -77,6 +77,39 @@ def test_explicit_empty_sudo_password_tries_empty_without_prompt(monkeypatch):
     assert sudo_stdin == "\n"
 
 
+def test_backend_nopasswd_probe_skips_interactive_prompt(monkeypatch):
+    monkeypatch.delenv("SUDO_PASSWORD", raising=False)
+    monkeypatch.setenv("HERMES_INTERACTIVE", "1")
+
+    def _fail_prompt(*_args, **_kwargs):
+        raise AssertionError("interactive sudo prompt should not run for NOPASSWD")
+
+    monkeypatch.setattr(terminal_tool, "_prompt_for_sudo_password", _fail_prompt)
+
+    transformed, sudo_stdin = terminal_tool._transform_sudo_command(
+        "sudo true",
+        sudo_nopasswd_check=lambda: True,
+    )
+
+    assert transformed == "sudo true"
+    assert sudo_stdin is None
+
+
+def test_configured_password_does_not_run_backend_nopasswd_probe(monkeypatch):
+    monkeypatch.setenv("SUDO_PASSWORD", "testpass")
+
+    def _fail_probe():
+        raise AssertionError("configured passwords must bypass the NOPASSWD probe")
+
+    transformed, sudo_stdin = terminal_tool._transform_sudo_command(
+        "sudo true",
+        sudo_nopasswd_check=_fail_probe,
+    )
+
+    assert transformed == "sudo -S -p '' true"
+    assert sudo_stdin == "testpass\n"
+
+
 def test_validate_workdir_blocks_shell_metacharacters_in_windows_paths():
     assert terminal_tool._validate_workdir(r"C:\Users\Alice\project; rm -rf /")
     assert terminal_tool._validate_workdir(r"C:\Users\Alice\project$(whoami)")

melounvitek/hermes-webdesktop

tests/tools/test_base_environment.py
@@ -6,6 +6,7 @@ init_session() failure handling, and the CWD marker contract.
 
 from unittest.mock import MagicMock
 
+import tools.terminal_tool_sudo as terminal_tool_sudo
 from tools.environments.base import BaseEnvironment
 from tools.environments.base_output import _BoundedOutputCollector
 
@@ -21,6 +22,48 @@ class _TestableEnv(BaseEnvironment):
 
     def cleanup(self):
         pass
+
+
+def test_prepare_command_uses_selected_environment_for_nopasswd(monkeypatch):
+    monkeypatch.delenv("SUDO_PASSWORD", raising=False)
+    monkeypatch.setenv("HERMES_INTERACTIVE", "1")
+    env = _TestableEnv()
+    monkeypatch.setattr(env, "_sudo_nopasswd_works", lambda: True)
+
+    def _fail_prompt(*_args, **_kwargs):
+        raise AssertionError("interactive sudo prompt should not run for NOPASSWD")
+
+    monkeypatch.setattr(terminal_tool_sudo, "_prompt_for_sudo_password", _fail_prompt)
+
+    transformed, sudo_stdin = env._prepare_command("sudo true")
+
+    assert transformed == "sudo true"
+    assert sudo_stdin is None
+
+
+def test_nopasswd_probe_runs_inside_selected_environment(monkeypatch):
+    env = _TestableEnv()
+    proc = object()
+    run = MagicMock(return_value=proc)
+    wait = MagicMock(return_value={"returncode": 0})
+    monkeypatch.setattr(env, "_run_bash", run)
+    monkeypatch.setattr(env, "_wait_for_process", wait)
+
+    assert env._sudo_nopasswd_works() is True
+    run.assert_called_once_with(
+        "sudo -n true",
+        login=False,
+        timeout=3,
+        stdin_data=None,
+    )
+    wait.assert_called_once_with(proc, timeout=3)
+
+
+def test_nopasswd_probe_fails_closed_on_backend_error(monkeypatch):
+    env = _TestableEnv()
+    monkeypatch.setattr(env, "_run_bash", MagicMock(side_effect=RuntimeError("offline")))
+
+    assert env._sudo_nopasswd_works() is False
 
 
 class TestBoundedOutputCollector:

tests/tools/test_terminal_tool.py
@@ -78,6 +78,39 @@ def test_explicit_empty_sudo_password_tries_empty_without_prompt(monkeypatch):
     assert sudo_stdin == "\n"
 
 
+def test_backend_nopasswd_probe_skips_interactive_prompt(monkeypatch):
+    monkeypatch.delenv("SUDO_PASSWORD", raising=False)
+    monkeypatch.setenv("HERMES_INTERACTIVE", "1")
+
+    def _fail_prompt(*_args, **_kwargs):
+        raise AssertionError("interactive sudo prompt should not run for NOPASSWD")
+
+    monkeypatch.setattr(terminal_tool_sudo, "_prompt_for_sudo_password", _fail_prompt)
+
+    transformed, sudo_stdin = terminal_tool_sudo._transform_sudo_command(
+        "sudo true",
+        sudo_nopasswd_check=lambda: True,
+    )
+
+    assert transformed == "sudo true"
+    assert sudo_stdin is None
+
+
+def test_configured_password_does_not_run_backend_nopasswd_probe(monkeypatch):
+    monkeypatch.setenv("SUDO_PASSWORD", "testpass")
+
+    def _fail_probe():
+        raise AssertionError("configured passwords must bypass the NOPASSWD probe")
+
+    transformed, sudo_stdin = terminal_tool_sudo._transform_sudo_command(
+        "sudo true",
+        sudo_nopasswd_check=_fail_probe,
+    )
+
+    assert transformed == "sudo -S -p '' true"
+    assert sudo_stdin == "testpass\n"
+
+
 def test_validate_workdir_blocks_shell_metacharacters_in_windows_paths():
     assert terminal_tool._validate_workdir(r"C:\Users\Alice\project; rm -rf /")
     assert terminal_tool._validate_workdir(r"C:\Users\Alice\project$(whoami)")